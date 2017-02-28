30f3eb0c99bf7d917f84769c8fb07f539ef32b43
Vladimir.Tyrin <b0-0b@yandex.ru>
2017-02-28 22:26:42 +0300

    Logging started

--- src/MinjustParser/Program.cs
+++ src/MinjustParser/Program.cs
@@ -1,12 +1,19 @@
 ﻿// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
+using ITCC.Logging.Core;
+
 namespace MinjustParser
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+        }
+
+        private static void InitLoggers()
+        {
+            Logger.Level = LogLevel.Trace;
         }
     }
 }
