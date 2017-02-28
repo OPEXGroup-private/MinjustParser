44cb381ede20225d8c8e84690fc90aaa3ace8190
Vladimir.Tyrin <b0-0b@yandex.ru>
2017-02-28 15:55:38 +0300

    Logger connected

--- src/MinjustParser/Program.cs
+++ src/MinjustParser/Program.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
 namespace MinjustParser
 {
